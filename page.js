bf08078cd216ea85931b9b95f8a643ca84466914
rosiearerosies <142237851+rosiearerosies@users.noreply.github.com>
2024-01-14 17:59:25 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,23 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function( event ) {
+
+    console.log( event );
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+}
+var recognition = new SpeechRecognition();
+recognition.start();
+console.log(event);
